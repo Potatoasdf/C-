44a855a57e6b90911f303dbfe63fe732e587e791
Potatoasdf <Aidan07perfect@gmail.com>
2023-02-15 09:13:28 -0800

    del and rehash + text stuff left

--- hash/linkedlist2/main.cpp
+++ hash/linkedlist2/main.cpp
@@ -3,14 +3,19 @@
 #include "node.h"
 using namespace std;
 void add(Node** &hash, int size);
-//void rehash()
+void rehash(Node** &hash, int &size);
 void print(Node** &hash, int size);
-//void del(Node* &head, int input, Node* &temp);
-void gpaAvg(Node** hash, int size, int count, float total);
+void del(Node** &hash, int size);
+void gpaAvg(Node** &hash, int size, int count, float total);
 int main() 
 {
   int size = 101;
   Node** hash = new Node*[size];
+  for (int i = 0; i< size; i++)
+    {
+      hash[i] = NULL;
+      
+    }
   bool alive = true;
   //while alive
   while (alive)
@@ -37,16 +42,14 @@ int main()
       {
         print(hash, size);
       }
-      /*
+      
       //if input is DELETE start void delete
       else if( strcmp(input, "DEL") == 0)
       {
-	int input;
-	cout << "ID" << endl;
-	cin >> input;
-        del(head, input, head);
+
+        del(hash, size);
       }
-      */
+      
       // return avg gpa
       else if(strcmp(input, "AVG") == 0)
       {
@@ -67,13 +70,13 @@ int main()
       {
         cout << "Please enter an actual input!" << endl;
       }
-      
+     
     }
 }
 //ascii is wrong
 void add(Node** &hash, int size)
 {
-  int count = 1;
+  int count = 0;
   Student* newstudent = new Student();
   newstudent->getInputs();
   newstudent->print();
@@ -84,6 +87,7 @@ void add(Node** &hash, int size)
     }
   else
     {
+      count++;
       Node* newnode = new Node(newstudent);
       cout << "IN" << endl;
       Node* temp = hash[newnode->getStudent()->acsii(size)];
@@ -98,6 +102,11 @@ void add(Node** &hash, int size)
       
       temp->setNext(newnode);
       cout << "C" << endl;
+    }
+  if (count == 4)
+    {
+      cout << "here" << endl;
+      rehash(hash, size);
     }
 
 }
@@ -120,57 +129,88 @@ void print(Node** &hash, int size)
 	}
     }
 }
-/*
-void del(Node* &head, int input, Node* &temp)
+
+void del(Node** &hash, int size)
 {
-  //if list empty
-  if(head == NULL)
+  bool confirm = false;
+  if (hash != NULL)
     {
-      cout << "you need students" << endl;
+      
+      int input;
+      cout << "ID: " << endl;
+      cin >> input;
+      for ( int i = 0; i < size; i++)
+	{
+	  if (hash[i] != NULL)
+	    {
+	    }
+	}
     }
-
-  // first student
-  else if(head->getStudent()->returnID() == input)
-    {
-      Node* holder = head;
-      temp = head->getNext();
-      holder->~Node();
-      head = temp;
-    }
-  //end of list
-  else if(temp->getNext() == NULL)
-    {
-      cout << "No more students" << endl;
-    }
-
-  // del if student has id, delete student then replace with student ahead
-  else if(temp->getNext()->getStudent()->returnID() == input)
-    {
-      Node* holder = temp->getNext()->getNext();
-      temp->getNext()->~Node();
-      temp->setNext(holder);
-    }
-  //recurse by going to next student
   else
     {
-      Node* holder = temp->getNext();
-      del(head, input, holder);
+      cout << "You need inputs to delete!" << endl;
     }
 }
 // get total of all gpa then divide by number of nodes
-*/
-void gpaAvg(Node** hash, int size, int count, float total)
+
+void gpaAvg(Node** &hash, int size, int count, float total)
 {
   for (int i = 0; i < size; i++)
     {
-      while (hash[i] != NULL)
+      Node* temp = hash[i];
+      while (temp != NULL)
 	{
-	  total += hash[i]->getStudent()->returnGPA();
+	  total += temp->getStudent()->returnGPA();
+	  cout << total << endl;
 	  count++;
-	  hash[i] = hash[i]->getNext();
+	  temp = temp->getNext();
 	}
     }
   cout << float((int)(((total/count)*100)+0.5))/100 << endl;
 
 }
 
+void rehash(Node** &hash, int &size)
+{
+  
+  size = size * 2;
+  Node** temp = new Node*[size];
+  for (int i = 0; i < size; i++)
+    {
+      temp[i] = NULL;
+    }
+  for (int i = 0; i < (size/2); i++)
+    {
+      if(hash[i] != NULL)
+	{
+	  Node* current = hash[i];
+	  while(current != NULL)
+	    {
+	      if(temp[current->getStudent()->acsii(size)] == NULL)
+		{
+		  cout << "BLAP" << endl;
+		  temp[current->getStudent()->acsii(size)] = current;
+		}
+	      else
+		{
+		  cout << "SMTHG" << endl;
+		  Node* endNode = temp[current->getStudent()->acsii(size)];
+                  cout << "HM" << endl;
+		  while (endNode->getNext() != NULL)
+		    {
+		      
+		      endNode = endNode->getNext();
+		      cout << "yey" << endl;
+		    }
+		  endNode->setNext(current);
+		  cout << "HM" << endl;
+		}
+	      current = current->getNext();
+	    }
+	}
+    }
+  cout << "WOW" << endl;
+  delete[] hash;
+  hash = temp;
+  
+}
