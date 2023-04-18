c3562fe34b7a77db80c8399f8de1a03b190bbb14
Potatoasdf <Aidan07perfect@gmail.com>
2023-04-18 11:02:07 -0700

    fix

--- ShuntingYard/treenode.cpp
+++ ShuntingYard/treenode.cpp
@@ -3,6 +3,7 @@
 
 treenode::treenode(char input)
 {
+  //constructor
   var = input;
   right = NULL;
   left = NULL;
@@ -13,21 +14,26 @@ treenode::~treenode()
 }
 treenode* treenode::getR()
 {
+  //returns right node
   return right;
 }
 void treenode::setR(treenode* newR)
 {
+  //give right node new val
   right = newR;
 }
 treenode* treenode::getL()
 {
+  //returns left node
   return left;
 }
 void treenode::setL(treenode* newL)
 {
+  //give left node new val
   left = newL;
 }
 char treenode::getVar()
 {
+  //return var
   return var;
 }
